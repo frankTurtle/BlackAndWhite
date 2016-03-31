2099f46ae70101adf41ac30828800e05e7004771
Barret J. Nobel <bear.nobel@gmail.com>
2016-03-31 19:55:50 -0400

    added color and currentLocation instance variables wrote constructor with params wrote setters / getters

--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,29 @@
 /**
  * Created by Barret J. Nobel on 3/31/2016.
  */
+
 public class Tile {
+    private char color;
+    private int currentLocation;
+
+    public Tile( char color, int currentLocation ){
+        this.setColor( color );
+        this.setCurrentLocation( currentLocation );
+    }
+
+    public void setColor( char color ){
+        this.color = color;
+    }
+
+    public void setCurrentLocation( int currentLocation ){
+        this.currentLocation = currentLocation;
+    }
+
+    public char getColor(){
+        return this.color;
+    }
+
+    public int getCurrentLocation(){
+        return this.currentLocation;
+    }
 }

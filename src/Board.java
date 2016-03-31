72725a5a203877054ab9828431cdd95ad0f7448f
Barret J. Nobel <bear.nobel@gmail.com>
2016-03-31 19:38:42 -0400

    started Board class, need to write Tile class first

--- src/Board.java
+++ src/Board.java
@@ -2,4 +2,10 @@
  * Created by Barret J. Nobel on 3/31/2016.
  */
 public class Board {
+    private char[] board = new char[7]; //... private instance variable to hold the board of chars
+    private boolean solved = false; //....... private instance variable to determine when board is solved
+
+    private Board(){
+
+    }
 }
